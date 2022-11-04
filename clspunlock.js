65dd28be5d7397cfb0804a3fa9f76ac952e6c34b
freeworldshadow <xiaomeili0117@gmail.com>
2022-11-05 00:23:58 +0800

    da

--- clspunlock.js
+++ clspunlock.js
@@ -1,7 +1,7 @@
 body = $response.body;
 obj = JSON.parse(body);
 if (obj["encData"] == "z3gsQqMKuyoE6wxhbU0B9r/Rtl245asPShKS+DFNyBa2c6pK7CuB44D9bgys4XGq") {
-    obj["encData"] = "tn7YgOJEDztxLt13qpqC7WV2U3HmczD6XQRanb2lIDcJ9N+08c0pE6Qnt76UCAFVBlSXZfSYiQIIVMaNRyYoJZfVEdRsXDlsLN1sCRCvkybG/d7wr+rHbgPM+/TZoIyH";
+    obj["encData"] = "tn7YgOJEDztxLt13qpqC7WV2U3HmczD6XQRanb2lIDdeOuMYeqHVZ2V/lIbzMjhkOuFyAX1sz2VmYFDqURCBi8bOfxC6l+sSQkky2VXv2lNcbALbPUvlcc1FocA1Dnmn";
 }
 body = JSON.stringify(obj);
 $done(body);
