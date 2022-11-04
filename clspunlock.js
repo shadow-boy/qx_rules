8f4c0b865c5960f51f59028b39ed6e91f98a93db
freeworldshadow <xiaomeili0117@gmail.com>
2022-11-05 00:49:48 +0800

    --

--- clspunlock.js
+++ clspunlock.js
@@ -1,7 +1,8 @@
 body = $response.body;
 obj = JSON.parse(body);
 if (obj["encData"] == "z3gsQqMKuyoE6wxhbU0B9r/Rtl245asPShKS+DFNyBa2c6pK7CuB44D9bgys4XGq") {
-    obj["encData"] = "tn7YgOJEDztxLt13qpqC7WV2U3HmczD6XQRanb2lIDdeOuMYeqHVZ2V/lIbzMjhkOuFyAX1sz2VmYFDqURCBi8bOfxC6l+sSQkky2VXv2lNcbALbPUvlcc1FocA1Dnmn";
+    // obj["encData"] = "tn7YgOJEDztxLt13qpqC7WV2U3HmczD6XQRanb2lIDdeOuMYeqHVZ2V/lIbzMjhkOuFyAX1sz2VmYFDqURCBi8bOfxC6l+sSQkky2VXv2lNcbALbPUvlcc1FocA1Dnmn";
+    obj["encData"] = "tn7YgOJEDztxLt13qpqC7WV2U3HmczD6XQRanb2lIDd3Pmagn0W6sE1C1vbBX5iUN08CQdqTD+iQrePrCNJoHt7y5W49gIvnHdpnSYWBqwk/yCTmir1Agp972eYPdrZR";
 }
 body = JSON.stringify(obj);
 $done(body);
