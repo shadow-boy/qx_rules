0363e8593df92454dff921aede2e65c3e1131351
shadowgg <wly1250623585@gmail.com>
2024-11-24 20:17:30 +0800

    Update replace_deadline.js

--- replace_deadline.js
+++ replace_deadline.js
@@ -3,7 +3,7 @@ let obj = JSON.parse(body);
 
 function addOneMonth(date) {
     let newDate = new Date(date);
-    newDate.setMonth(newDate.getMonth() + 1);
+    newDate.setMonth(newDate.getMonth() + 12);
     return newDate;
 }
 
@@ -13,6 +13,9 @@ let newDeadline = addOneMonth(currentDate);
 // 更新 deadline 和 isoDeadline
 obj.data.deadline = newDeadline.toISOString().replace('T', ' ').split('.')[0]; // 格式化为 "YYYY-MM-DD HH:mm:SS"
 obj.data.isoDeadline = newDeadline.toISOString(); // 格式化为 ISO 时间格式
+
+obj.data.quotaConsumed = "641563139";
+obj.data.expired = true;
 
 body = JSON.stringify(obj);
 $done({ body });
